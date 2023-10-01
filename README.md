ab1f6ec98d2506b0818d8f5a1ae7c3a94931ca50
thnhtra2001 <trab1910315@student.ctu.edu.vn>
2023-10-01 22:35:57 +0700

    Khoi tao du an shop ban cay canh

---
+++ README.md
@@ -0,0 +1,3 @@
+echo "# shopcaycanh"
+- MSSV: B1910315
+- Ho va ten: Tran Thanh Tra
---
+++ android/app/src/main/kotlin/com/example/shopcaycanh/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.shopcaycanh
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
